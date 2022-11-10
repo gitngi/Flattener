e7a7195384cd4e0f7a5b66d9267e65e7df1cb26f
gitngi <99325104+gitngi@users.noreply.github.com>
2022-11-10 16:12:00 +0100

    Add files via upload

---
+++ project.py
@@ -0,0 +1,60 @@
+import json
+import sys
+
+
+def my_flatten(juanson: json):
+    flat = {}
+    print(f"The json input: {juanson}", end="\n\n")
+
+    # this is only to print in levels, easier to follow recursion
+    tab = "    "
+
+    def recur(element, flat, key='', this_level=0):
+       # print(
+        # f"{this_level*tab}element: {element},   flat: {flat},    key: {key},  level: {this_level}")
+
+        if isinstance(element, dict):
+            items = element.items()
+           # print(f"{this_level*tab}Items from dictionary: {items}")
+
+            for k, v in items:
+
+                new_level = this_level + 1
+                if key == "":
+                    new_key = k
+                else:
+                    new_key = f"{key}.{k}"
+
+                recur(v, flat, new_key, new_level)
+
+        elif isinstance(element, list):
+            #print(f"{this_level*tab}Items from list: {list(enumerate(element))}")
+
+            for index, v in enumerate(element):
+
+                new_level = this_level + 1
+                if key == "":
+                    new_key = index
+                else:
+                    new_key = f"{key}.{index}"
+                recur(v, flat, new_key, new_level)
+        else:
+            flat[key] = element
+        return
+
+    recur(juanson, flat)
+
+    return flat
+
+
+print('type the json that you wish to flatten here:')
+user_input = ''
+for line in sys.stdin:
+    if line == '\n':
+        break
+    user_input += line
+
+to_parse = json.loads(user_input)
+resulted_flat = my_flatten(to_parse)
+print('the flattened json output: ')
+print(resulted_flat)
---
+++ test_file.py
@@ -0,0 +1,56 @@
+import unittest
+import scripts.project as file
+
+
+class Test_my_flatten(unittest.TestCase):
+    def test_my_flat(self):
+        dic = {
+            "a": 1,
+            "b": 2,
+            "c": [{"d": [2, 3, 4], "e": [{"f": 1, "g": 2}]}]}
+
+        expected = {'a': 1,
+                    'b': 2,
+                    'c.0.d.0': 2,
+                    'c.0.d.1': 3,
+                    'c.0.d.2': 4,
+                    'c.0.e.0.f': 1,
+                    'c.0.e.0.g': 2}
+
+        real = file.my_flatten(dic)
+        self.assertNotEqual(real, expected)
+
+    def test_my_flat_1(self):
+        dic = {
+            "a": 1,
+            "b": 2,
+            "c": [{"d": [2, 3, 4], "e": [{"f": 1, "g": 2}]}]}
+
+        expected = {'a': 1,
+                    'b': 2,
+                    'c.d.0': 2,
+                    'c.d.1': 3,
+                    'c.d.2': 4,
+                    'c.e.f': 1,
+                    'c.0.e.0.g': 2}
+
+        real = file.my_flatten(dic)
+        self.assertNotEqual(real, expected)
+
+    def test_my_flat_1(self):
+        dic = {
+            "a": 1,
+            "b": True,
+            "c": {"d": 3,
+                  "e": 'test'
+                  }
+        }
+
+        expected = {'a': 1,
+                    'b': True,
+                    'c.d': 3,
+                    'c.e': 'test',
+                    }
+
+        real = file.my_flatten(dic)
+        self.assertEqual(real, expected)
